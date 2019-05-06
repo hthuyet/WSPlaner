01f9c1c3a38e0603e7e1a3ae8bf84713b2f0d8fd
thuyetlv <lethuyet.10.11.1990@gmail.com>
2019-05-06 20:51:45 +0700

    update url iframe

--- webapp/src/main/resources/static/wsplanning/js/calendarView/CalendarViewCtrl.controller.js
+++ webapp/src/main/resources/static/wsplanning/js/calendarView/CalendarViewCtrl.controller.js
@@ -1,5 +1,7 @@
 UserWebApp.controller('CalendarViewCtrl', function ($scope, $rootScope, $locale, HttpService, $translate, $location, $state, $sce, $uibModal, CommonServices) {
 
+    //https://automaster.alliedsoft.hu:10106/default.aspx?siteid=102&deptid=201&licno=&smanid=0&shiftId=21&daysShowing=7
     var EmployeeData = $("#EmployeeData").data("employee");
-    $scope.iframeUrl = $sce.trustAsResourceUrl(constants.URL_IFRAME + EmployeeData.SiteId);
+    console.log(EmployeeData);
+    $scope.iframeUrl = $sce.trustAsResourceUrl(constants.URL_IFRAME + EmployeeData.SiteId + "&deptid=" + EmployeeData.DeptId+ "&shiftId=" + EmployeeData.ShiftId);
 });
