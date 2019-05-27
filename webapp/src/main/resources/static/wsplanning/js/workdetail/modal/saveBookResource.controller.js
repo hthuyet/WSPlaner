559b78817e241b6bdd7c2910c71b1e8cb2da9b46
thuyetlv <lethuyet.10.11.1990@gmail.com>
2019-05-27 21:10:40 +0700

    update time input

--- webapp/src/main/resources/static/wsplanning/js/workdetail/modal/saveBookResource.controller.js
+++ webapp/src/main/resources/static/wsplanning/js/workdetail/modal/saveBookResource.controller.js
@@ -8,18 +8,19 @@ UserWebApp.controller('SaveBookPoolResourceCtrl', function ($scope, $rootScope, 
   console.log($ctrl.data);
 
   $ctrl.save = function () {
-    var dateStart = new Date($ctrl.data.Date.getTime());
-    var dateEnd = new Date($ctrl.data.Date.getTime());
-
-    var tmp = $ctrl.data.sStart.split(":");
-    dateStart.setHours(parseInt(tmp[0]));
-    dateStart.setMinutes(parseInt(tmp[1]));
-    $ctrl.data.StartTime = dateStart;
-
-    tmp = $ctrl.data.sEnd.split(":");
-    dateEnd.setHours(parseInt(tmp[0]));
-    dateEnd.setMinutes(parseInt(tmp[1]));
-    $ctrl.data.EndTime = dateEnd;
+    //
+    // var dateStart = new Date($ctrl.data.Date.getTime());
+    // var dateEnd = new Date($ctrl.data.Date.getTime());
+    //
+    // var tmp = $ctrl.data.sStart.split(":");
+    // dateStart.setHours(parseInt(tmp[0]));
+    // dateStart.setMinutes(parseInt(tmp[1]));
+    // $ctrl.data.StartTime = dateStart;
+    //
+    // tmp = $ctrl.data.sEnd.split(":");
+    // dateEnd.setHours(parseInt(tmp[0]));
+    // dateEnd.setMinutes(parseInt(tmp[1]));
+    // $ctrl.data.EndTime = dateEnd;
 
     $uibModalInstance.close($ctrl.data);
   };
