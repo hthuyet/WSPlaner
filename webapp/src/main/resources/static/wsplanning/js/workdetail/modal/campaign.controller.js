371697b2b3bf97203904674cca49b8ab2c029719
jackytran <bachtuvu.dev@gmail.com>
2019-07-29 00:13:28 +0700

    jacky v4 28072019

--- webapp/src/main/resources/static/wsplanning/js/workdetail/modal/campaign.controller.js
+++ webapp/src/main/resources/static/wsplanning/js/workdetail/modal/campaign.controller.js
@@ -1,10 +1,8 @@
 UserWebApp.controller('CampaignModalCtrl', function ($scope, item, HttpService, $translate, $location, $filter, $uibModal, $uibModalInstance, item) {
 
   var $ctrl = this;
-  $ctrl.item = item;
-  $ctrl.selected = item;
 
-  $scope.VehicleNotifications = item.VehicleNotifications;
+  $scope.VehicleNotifications = item;
 
   $ctrl.ok = function () {
     $uibModalInstance.close($ctrl.selected);
