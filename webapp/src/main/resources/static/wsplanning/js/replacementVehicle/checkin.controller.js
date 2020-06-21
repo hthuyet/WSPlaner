24377dd5ba72672eba8fb7a8fbc60e12fc50c0e3
thuyetlv <lethuyet.10.11.1990@gmail.com>
2020-06-21 15:00:46 +0700

    add replacmence

--- webapp/src/main/resources/static/wsplanning/js/replacementVehicle/checkin.controller.js
+++ webapp/src/main/resources/static/wsplanning/js/replacementVehicle/checkin.controller.js
@@ -8,6 +8,17 @@ UserWebApp.controller('ReplacementCheckInCtrl', function ($scope, $rootScope, $l
     "vehicle": "",
   };
 
+  $scope.fuelList = [
+    { "Id": "01", "Name": "1/4" },
+    { "Id": "02", "Name": "2/4" },
+    { "Id": "03", "Name": "3/4" },
+    { "Id": "04", "Name": "4/4" },
+      ];
+  $scope.attachmentTypes = [
+      { "Id": "01", "Name": "Driver License" },
+      { "Id": "02", "Name": "Vehicle Picture" }
+    ];
+
   function loadCommon() {
     CommonServices.getTransactionTypes().then(function (data) {
       $scope.lstTrans = data;
@@ -94,6 +105,104 @@ UserWebApp.controller('ReplacementCheckInCtrl', function ($scope, $rootScope, $l
     "width": 900,
     "height": 336,
   };
+
+  $scope.$watch("color", function (newValue, oldValue) {
+    var modified = false;
+    if (newValue != oldValue) {
+      console.log(newValue);
+      $timeout(function () {
+        angular.element('#btnUpdateColor').triggerHandler('click');
+      });
+      modified = true;
+    } else if (newValue != "green") {
+      modified = true;
+    } else {
+      modified = false;
+    }
+    $scope.$emit('isSave', {
+          modified: modified,
+        }
+    );
+  });
+
+  function createListWOAttachment() {
+    var list = [];
+    var i = 0;
+    //template da danh dau
+    var template = $scope.acceptTemplate();
+    if (!template.isEmpty) {
+      var templateMark = {};
+      templateMark.FileId = 0;
+      templateMark.FileName = $scope.templateName;
+      templateMark.dataUrl = template.dataUrl;
+      templateMark.ImageData = template.dataUrl.replace("data:image/png;base64,", "");
+      templateMark.AttachType = "VHC";
+      templateMark.AttachTypeDescription = "";
+      templateMark.FileDescription = "";
+      list[i++] = templateMark;
+    }
+
+    var signCanvas = $scope.accept();
+    if (!signCanvas.isEmpty) {
+      var sign = {};
+      sign.FileId = 0;
+      sign.FileName = "Signature_of_customer.png";
+      sign.dataUrl = signCanvas.dataUrl;
+      sign.ImageData = signCanvas.dataUrl.replace("data:image/png;base64,", "");
+      sign.AttachType = "SIGNATUREP";
+      sign.AttachTypeDescription = "";
+      sign.FileDescription = "";
+      list[i++] = sign;
+    }
+
+    if ($scope.listImage && $scope.listImage.length > 0) {
+      Array.prototype.push.apply(list, $scope.listImage);
+    }
+
+    return list;
+  }
+
+  $scope.onSubmitForm = function (params) {
+
+    console.log("-----onSubmitForm---");
+    var list = createListWOAttachment();
+
+    console.log(list);
+
+    common.btnLoading($(".btnSubmit"), true);
+    // WorkOrderService.postWorkOrder(data, postAction).then(function (res) {
+    //   common.btnLoading($(".btnSubmit"), false);
+    //   console.log(res);
+    //   if (res.data.Token && res.data.Token.ErrorDesc) {
+    //     common.notifyWithMessage("Warning!!!", res.status, res.data.Token.ErrorDesc)
+    //   } else {
+    //     common.notifySuccess("Success!!!");
+    //   }
+    //   console.log($scope.WorkOrder);
+    //
+    //   if (params) {
+    //     $scope.$emit('isSave', {
+    //
+    //           modified: false,
+    //         }
+    //     );
+    //     console.log(params);
+    //     console.log($state.current);
+    //     $state.transitionTo($state.current, params, {
+    //       reload: false, inherit: false, notify: false, location: "replace"
+    //     });
+    //   } else {
+    //     if ($scope.WorkOrder && $scope.WorkOrder.WorkOrderId) {
+    //       location.reload();
+    //     }
+    //   }
+    //
+    // }, function (err) {
+    //   common.btnLoading($(".btnSubmit"), true);
+    //   console.log(err);
+    //   common.notifyError("Error!!!", err.status);
+    // });
+  }
 
 
   //<editor-fold desc="openImage">
